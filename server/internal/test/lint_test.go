fbfa90883e34edc251bee00162b0abde80f69c12
Sven Hohlfeld <sho1975@gmail.com>
2025-05-05 19:05:27 +0200

    fix: try to remove compile errors while still having linting

--- server/internal/test/lint_test.go
+++ server/internal/test/lint_test.go
@@ -7,7 +7,6 @@ import (
 
 // TestLintIssues is a test with intentional linting issues
 func TestLintIssues(t *testing.T) {
-	var unusedVar string // should trigger unused variable warning
 	fmt.Println("This is a test with linting issues")
 
 	// should trigger error handling warning
